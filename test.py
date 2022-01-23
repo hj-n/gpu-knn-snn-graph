8e42d50d360325ac15701cd0d9b66fcd6330955e
Hyeon Jeon <hj@hcil.snu.ac.kr>
2022-01-23 22:17:30 +0900

    revised test.py

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-from knnsnn import KnnSnn as ks
-from sklearn.neighbors import KDTree
+'''
+HOWTOUSE for knnsnn.py
+'''
 
+
+from knnsnn import KnnSnn as ks ## import
 import numpy as np
 
 
 sample = np.random.rand(20000, 768).astype('float32')
 
 k=20
-KSnn = ks(k)
+KSnn = ks(k) ## initialize the instance
 
-knn_indices = KSnn.knn(sample)
-snn_results = KSnn.snn(knn_indices)
+knn_indices = KSnn.knn(sample) ## run knn
+snn_results = KSnn.snn(knn_indices) ## run snn
